36949acfcf4dc59935efa607babcef2bebc25c12
Md Sazzad Hossain <m.sazzad.h3@gmail.com>
2026-05-20 17:06:58 +0600

    Initial ReviseQ Android setup

---
+++ app/src/main/java/com/reviseq/android/core/network/NetworkConstants.kt
@@ -0,0 +1,5 @@
+package com.reviseq.android.core.network
+
+object NetworkConstants {
+    const val BASE_URL = "http://192.168.0.179:8000/"
+}
---
+++ app/src/main/java/com/reviseq/android/data/remote/dto/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.reviseq.android.data.remote.dto
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
